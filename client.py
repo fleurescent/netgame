1038ab2e693d081dfad00b5592ae131fb626cd3f
fleurescent <52236895+fleurescent@users.noreply.github.com>
2021-02-19 22:05:46 -0800

    Client now stays open after one client is closed

--- client.py
+++ client.py
@@ -135,4 +135,25 @@ def main():
         
         redraw_window(win, game, player)
 
-main()
+def menu_screen():
+    run = True
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(60)
+        win.fill((128, 128, 128))
+        font = pygame.font.SysFont('arialrounded', 60)
+        text = font.render('Click to Play!', 1, (255, 0, 0))
+        win.blit(text, (100, 200))
+        pygame.display.update()
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                run = False
+    main()
+
+while True:
+    menu_screen()
